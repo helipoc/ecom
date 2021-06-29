691ef93bfdadda746ae988ed9a791aa694e654aa
helipoc <klonja0x@gmail.com>
2021-06-29 20:09:21 +0100

    Production build

---
+++ public/precache-manifest.246163d308c7a274305db9643d2f4235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139d9546b6326015ee520e33bc3f312d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d189243e7804a0883df7",
+    "url": "/static/css/main.5a06845b.chunk.css"
+  },
+  {
+    "revision": "9a774dfe9509fb31960c",
+    "url": "/static/js/2.1e44d7f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1e44d7f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d189243e7804a0883df7",
+    "url": "/static/js/main.268012cb.chunk.js"
+  },
+  {
+    "revision": "7646288671f8458d76de",
+    "url": "/static/js/runtime-main.49627031.js"
+  }
+]);
