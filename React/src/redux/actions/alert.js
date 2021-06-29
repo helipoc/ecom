ab1a2144690a816f8626b2fd06581fe570e9110c
helipoc <klonja0x@gmail.com>
2021-06-29 20:13:39 +0100

    removed trash files

--- React/src/redux/actions/alert.js
+++
@@ -1,4 +0,0 @@
-import { AlertTypes as Types } from "../Types";
-export function setAlert(msg, sev) {
-  return { type: Types.SET_ALERT, alert: { msg, sev } };
-}
--- React/src/redux/actions/items.js
+++
@@ -1,9 +0,0 @@
-import { ItemTypes as Types } from "../Types";
-
-export function ItemsLoaded(data) {
-  return { type: Types.ITEMS_LOADED, data };
-}
-
-export function ItemDeleted(id) {
-  return { type: Types.ITEM_DELETED, id };
-}
